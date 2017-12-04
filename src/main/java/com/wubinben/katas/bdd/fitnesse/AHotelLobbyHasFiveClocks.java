e4196132743cee02c716374f92389ea6f77a9d7a
Ben Wu <wubin28@gmail.com>
2017-12-04 15:58:25 +0800

    make the first error 'could not invoke constructor' green

---
+++ src/main/java/com/wubinben/katas/bdd/fitnesse/AHotelLobbyHasFiveClocks.java
@@ -0,0 +1,13 @@
+package com.wubinben.katas.bdd.fitnesse;
+
+/**
+ * FitNesse Fixture Code
+ * User: Ben
+ * Date: 13-9-11
+ * Time: 上午9:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class AHotelLobbyHasFiveClocks {
+    public AHotelLobbyHasFiveClocks() {
+    }
+}
