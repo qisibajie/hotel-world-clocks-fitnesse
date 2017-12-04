5daf6ca333e7efba62933e8283daa5db8f5f47cb
Ben Wu <wubin28@gmail.com>
2017-12-04 15:44:18 +0800

    create a blank project

---
+++ src/main/java/com/wubinben/katas/bdd/fitnesse/App.java
@@ -0,0 +1,13 @@
+package com.wubinben.katas.bdd.fitnesse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
