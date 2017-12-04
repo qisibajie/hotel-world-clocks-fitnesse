560fbb030b4690aea09eaffee455254c21de6f4d
Ben Wu <wubin28@gmail.com>
2017-12-04 16:01:12 +0800

    make all fitnesse wiki tests green

--- src/main/java/com/wubinben/katas/bdd/fitnesse/AHotelLobbyHasFiveClocks.java
+++ src/main/java/com/wubinben/katas/bdd/fitnesse/AHotelLobbyHasFiveClocks.java
@@ -8,6 +8,40 @@ package com.wubinben.katas.bdd.fitnesse;
  * To change this template use File | Settings | File Templates.
  */
 public class AHotelLobbyHasFiveClocks {
+
+    private HotelWorldClocksFacade hotelWorldClocksFacade;
+
     public AHotelLobbyHasFiveClocks() {
+        this.hotelWorldClocksFacade = HotelWorldClocksFacade.newInstance();
+        this.hotelWorldClocksFacade.testInitialization();
+    }
+
+    public void SetCityToBeInTheDSTPeriod(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.ACTIVE);
+
+    }
+
+    public void SetCityToBeNotUsingTheDST(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.INACTIVE);
+    }
+
+    public void AdjustTheIncorrectTimeOfTheClockOfCityToBeTheCorrectHour(String cityName, int hour) {
+        this.hotelWorldClocksFacade.adjustIncorrectTimeOfCity(hour, cityName);
+    }
+
+    public String theTimeOfTheClockOfCityShouldBe(String cityName) {
+        return this.hotelWorldClocksFacade.getResultHour(cityName);
+    }
+
+    public void EndTheDSTPeriodOfCity(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.INACTIVE);
+    }
+
+    public void ContinueTheDSTPeriodOfCity(String cityName) {
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.ACTIVE);
+    }
+
+    public void AdjustTheTimeOfTheClockOfCityDueToTheEndOfDSTToBe(String cityName, int hour) {
+        this.hotelWorldClocksFacade.adjustIncorrectTimeOfCity(hour, cityName);
     }
 }
